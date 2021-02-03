fb21e66af05145433eb232608d48c74c77a60804
Dipta Sikder <anotheramit8@gmail.com>
2021-02-03 16:15:12 +0600

    Learned about Spread operator

---
+++ threedots.js
@@ -0,0 +1,20 @@
+const ages = [12, 14, 16, 13, 17];
+const ages2 = [15, 16, 12];
+const ages3 = [25, 36, 22, 29];
+//before es-6
+const allAges = ages.concat(ages2).concat([5]).concat(ages3);
+//after es-6
+const allAges2 = [...ages, ...ages2, 5, ...ages3];
+//console.log(allAges2); 
+
+
+
+const business = 650;
+const minister = 450;
+const sochib = 250;
+const takaPoisa = [650, 450, 250, 850];
+// const maximum = Math.max(business, minister, sochib);
+
+//Comparing elements of array
+const maximum = Math.max(...takaPoisa);
+console.log(maximum);
