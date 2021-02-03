3d356e19f4ec4f6a08671c6bee3d93a891030c30
Dipta Sikder <anotheramit8@gmail.com>
2021-02-03 16:29:31 +0600

    Learned about class, constructor,creating obj. from class

---
+++ class.js
@@ -0,0 +1,15 @@
+//class
+class Student{
+    //constructor
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "kolimunnesa school"
+    }
+}
+
+//creating obj.
+const student1 = new Student(12, "Shuvo");
+const student2 = new Student(22, "mahiya");
+const student3 = new Student(29, "Bappi");
+console.log(student3);
